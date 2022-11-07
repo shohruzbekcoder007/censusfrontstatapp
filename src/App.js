e46cab79794484e16866af01b6a69489a17e6f0f
shohruzbekcoder <shohruzbekochilov007@gmail.com>
2022-11-07 22:24:02 +0500

    react-query and axios installed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React from "react"
 import defaultTheme from './theme/defaultTheme'
-import ResponsiveDrawer from './components/Main';
+import ResponsiveDrawer from './components/Main'
 import {
   BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
-import Panel from './components/Panel';
+import Panel from './components/Panel'
 import { ThemeProvider  } from 'styled-components'
+import { QueryClient, QueryClientProvider} from 'react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={defaultTheme}>
-          <Routes>
-              <Route path="/" element={<ResponsiveDrawer/>}>
-                  <Route index element={<Panel/>}/>
-                  <Route path="surovnoma" element={<p>salom2</p>} />
-                  <Route path="sozlamalar" element={<p>salom3</p>} />
-              </Route>
-          </Routes>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider theme={defaultTheme}>
+            <Routes>
+                <Route path="/" element={<ResponsiveDrawer/>}>
+                    <Route index element={<Panel/>}/>
+                    <Route path="surovnoma" element={<p>salom2</p>} />
+                    <Route path="sozlamalar" element={<p>salom3</p>} />
+                </Route>
+            </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
     
   );
 }
---
+++ src/utils/API_urls.js
@@ -0,0 +1,5 @@
+// host
+export const host = "http://localhost:8080"
+
+// user
+export const user = `/user`
