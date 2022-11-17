be6810b186688a03f06280fadc04810db0090bd4
shohruzbekcoder <shohruzbekochilov007@gmail.com>
2022-11-17 22:59:14 +0500

    final page finished

--- src/components/QuestionnaireTabFour/index.jsx
+++ src/components/QuestionnaireTabFour/index.jsx
@@ -1,6 +1,9 @@
+import { Button } from '@mui/material'
 import React from 'react'
+import Heading1 from '../../helper_components/Heading1'
+import Heading2 from '../../helper_components/Heading2'
 import Stepper from '../Stepper'
-import { FinalContainer, FinalWrapper } from './styles'
+import { FinalContainer, FinalWrapper, FinalContent, FinalCode, FinalImg } from './styles'
 
 export default function QuestionnaireTabFour() {
   return (
@@ -8,7 +11,35 @@ export default function QuestionnaireTabFour() {
         <Stepper activeItem={4} />
         <FinalWrapper>
             <FinalContainer>
-                salom
+                <Heading1 color="blue" size="4em">
+                  So’rovnomangiz muvaffaqiyatli yuborildi! 
+                </Heading1>
+                <Heading1 color="black" size="4em">
+                  So’rovnomada qatnashganingiz uchun rahmat!
+                </Heading1>
+                <FinalContent>
+                  So’rovnomalar tasdiqlash uchun topshirilganidan so’ng ularni o’zgartirish va ularni ko’zdan kechirish imkoningiz bo’lmaydi. Topshirilgan savolnomalar so’rovnomalar ro’yxatida paydo bo’lgandan so’ng browser oynasini yopishingiz mumkin! So’rovnomada ishtirokingiz uchun tashakkur etamiz!
+                </FinalContent>
+                <FinalCode>
+                  <Heading2 color="black" size="3em">Tasdiqlash kodi: 12335</Heading2>
+                </FinalCode>
+                <FinalImg>
+                  <img src={require('../../images/people.png')} alt="people" />
+                </FinalImg>
+                <Button
+                            variant="contained"
+                            color="primary"
+                            sx={{
+                                borderRadius: "5px",
+                                padding: "12px 20px",
+                                fontWeight: "700",
+                                fontSize: "1.75em",
+                                lineHeight: "21px",
+                                textTransform: "inherit"
+                            }}
+                        >
+                            To'ldirilgan so'rovnomani ko'rish
+                        </Button>
             </FinalContainer>
         </FinalWrapper>
     </>
--- src/components/QuestionnaireTabFour/styles.js
+++ src/components/QuestionnaireTabFour/styles.js
@@ -12,4 +12,35 @@ export const FinalContainer = styled.div`
     width: 1336px;
     margin: 0 auto;
     background-image: url(${flag});
+    text-align: center;
+    @media (max-width: 1920px) {
+        width: 100%;
+        padding: 40px 20px;
+    }
 `
+
+export const FinalContent = styled.p`
+    color: ${props => props.theme.color.black};
+    font-style: ${props => props.theme.text.font_style};
+    line-height: ${props => props.theme.text.line_height};
+    font-weight: ${props => props.theme.text.big_text.font_weight};
+    font-size: ${props => props.theme.text.big_text.font_size};
+    text-transform: ${props => props.theme.text.big_text.text_transform};
+    padding-top: 26px;
+    padding-bottom: 21px;
+`
+
+export const FinalCode = styled.div`
+    padding-bottom: 20px;
+`;
+
+export const FinalImg = styled.div`
+    width: 100%;
+    height: auto;
+    text-align: center;
+    padding: 0 20px;
+    margin-bottom: 20px;
+    img {
+        width: 100%
+    }
+`;
